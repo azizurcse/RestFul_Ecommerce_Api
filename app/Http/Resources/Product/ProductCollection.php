24b9c4e96776d74b77e24906b9ee5644f455b465
azizurcse <azizur.jibon@gmail.com>
2018-02-26 13:02:07 +0600

    store api review resource and product pagination

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -17,7 +17,7 @@ class ProductCollection extends Resource
         return [
             'name'=>$this->name,
             'totalPrice'=>round((1-($this->discount/100)) * $this->price,2),
-            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count()) : 'no rating yet',
+            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'no rating yet',
             'discount'=>$this->discount,
             'link'=>route('products.show',$this->id)
         ];
